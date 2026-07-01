b11d95c3e925e2867c9782ae06419eea7d4e2d42
Yifei Sun <ysun@hey.com>
2026-07-01 16:42:22 +0200

    auto find root

--- formatter.nix
+++ formatter.nix
@@ -8,7 +8,19 @@
 writeShellScriptBin "formatter" ''
   set -eoux pipefail
   shopt -s globstar
+
+  root="$PWD"
+  while [[ ! -f "$root/.git/index" ]]; do
+    if [[ "$root" == "/" ]]; then
+      exit 1
+    fi
+    root="$(dirname "$root")"
+  done
+  pushd "$root" > /dev/null
+
   # ${lib.getExe deno} fmt readme.md
   ${lib.getExe nixpkgs-fmt} .
   ${lib.getExe typstyle} --inplace **/*.typ
+
+  popd
 ''
